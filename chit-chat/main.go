bca3091f74b7b6b3b7734a01e33a90a0a5db6e6c
Shukurdin <ShukurdinAi@halykban.kz>
2020-03-05 16:26:12 +0600

    temp

---
+++ chit-chat/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+	"time"
+	//"./data"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	threads, err := data.Threads()
+	if err != nil {
+		return
+	}
+	_, err = session(writer, request)
+	if err != nil {
+		generateHtml(writer, threads, "layout", "public.navbar", "index")
+	} else {
+		generateHtml(writer, threads, "layout", "private.navbar", "index")
+	}
+}
+
+func generateHtml(w http.ResponseWriter, data interface{}, templates ...string) {
+	var files []string
+	for _, file := range templates {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	t := template.Must(template.ParseFiles(files...))
+	t.ExecuteTemplate(w, "layout", data)
+}
+
+func err(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func login(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func logout(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func signup(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func signupAccount(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func newThread(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func createThread(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func postThread(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func readThread(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+type Session struct {
+	Id        int
+	Uuid      string
+	Email     string
+	UserId    int
+	CreatedAt time.Time
+}
---
+++ chit-chat/util.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"./data"
+	"net/http"
+)
+
+func session(writer http.ResponseWriter, request *http.Request) (session data.Session, err error) {
+	cookie, err := request.Cookie("_cookie")
+	if err == nil {
+		session = data.Session{Uuid: cookie.Value}
+		if ok, _ := session.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
